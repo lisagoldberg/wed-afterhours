2c00eedc24d62cab64b2d1e80d215371e644d6b0
Adam Wright <leurcutis@gmail.com>
2016-03-09 19:00:08 -0800

    for and while loops

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var friends=['Erik', 'Jenny', 'Sean', 'Ilya', 'David'];
+
+
+for( i = 0; i < friends.length; i++) {
+  console.log(friends[i]);
+}
+
+var otherFriends=['Phil', 'Kate', 'John', 'Matt', 'Jude'];
+var x = 0
+while(x < otherFriends.length){
+  console.log(otherFriends[x]);
+
+  x++
+}
+
